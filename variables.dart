9029121e233a5ac7efbfbffb50bbcb3d0b3a4070
PabloPascucci <pascuccipablo20@gmail.com>
2025-01-20 02:29:16 -0300

    Variables

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Tipos de variables
+
+  int edad = 10;
+  double temperatura = 18.5;
+  String nombre = 'Pablo';
+  bool estado = true;
+  String cualquierCosa;
+
+  cualquierCosa = 'Texto Cualquiera';
+}
